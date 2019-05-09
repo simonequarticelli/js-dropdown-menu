d63e6a97a54e278cf462ad9e66b5d83edff44546
simonequarticelli <quarti87@gmail.com>
2019-05-10 00:13:45 +0200

    esercizio terminato

--- main.js
+++ main.js
@@ -1,31 +1,18 @@
 $(document).ready(function(){
+//menu
+$('.menu__item').mouseenter(function(){
+  $(this).children('.dropdown__item').addClass('active');
+});
 
+$('.menu__item').mouseleave(function(){
+  $(this).children('.dropdown__item').removeClass('active');
+});
+//sottomenu
+$('.first').mouseenter(function(){
+  $('.sottomenu').addClass('active');
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
+$('.first').mouseleave(function(){
+  $('.sottomenu').removeClass('active');
+});
 });
